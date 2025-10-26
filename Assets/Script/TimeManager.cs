3126d42ab83d4b2d9a030f27df14ec3841d3ac4e
hoangduc170803 <hoangduc170803@gmail.com>
2025-10-26 10:00:00 +0700

    feat: implement time management and player movement

--- Assets/Script/TimeManager.cs
+++ Assets/Script/TimeManager.cs
@@ -3,7 +3,7 @@
 public class TimeManager : MonoBehaviour
 {
     public static TimeManager Instance;
-    public float realSecondsPerGameMinute = 6f; // 60 min * 6s = 360s = 6 phút (tùy chỉnh)
+    public float realSecondsPerGameMinute = 0.4167f;
     private float timer;
     public int gameHour = 6;
     public int gameMinute = 0;
@@ -20,21 +20,22 @@ public class TimeManager : MonoBehaviour
         {
             timer = 0;
             gameMinute++;
-
             if (gameMinute >= 60)
             {
                 gameMinute = 0;
                 gameHour++;
                 GameManager.Instance.ChangeStamina(-1);
             }
-
             if (gameHour >= 24)
             {
                 gameHour = 0;
-                GameManager.Instance.EndDay();
             }
-
             UIManager.Instance.UpdateTimeUI();
         }
+    }
+
+    public float GetGameTimeInSeconds()
+    {
+        return (gameHour * 3600) + (gameMinute * 60);
     }
 }
